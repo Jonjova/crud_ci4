7a7c37f820f6a335e014aaf0c20efb48160c65e5
Jonathan <jonjova1992@hotmail.com>
2022-09-11 14:37:09 -0600

    añadiendo funcionalidad de tabla cheq

--- assets/js/product.js
+++ assets/js/product.js
@@ -15,35 +15,6 @@ $(".chall").click(function () {
 });
 
 // aqui termina funcionalidad para seleccion multiple
-// for (let index = 0; index < nivel1.length; index++) {
-//   console.log(nivel1[index] + index);
-
-// $(".ch-1").click(function () {
-//   // aquí empieza la funcionalidad para dar selección dentro de una celda
-//   $(".ch1").prop("checked", $(this).prop("checked"));
-//   // $(".ch1").prop('disabled', false);
-//   // if (!$(this).prop("checked")) {
-//   //     $('.ch1').prop("disabled", true);
-//   // }
-// });
-
-// $(".ch1").click(function () {
-//   if (!$(this).prop("checked")) {
-//     $(".ch-1").prop("checked", false);
-//   }
-// });
-
-// $(".ch-2").click(function () {
-//   $(".ch2").prop("checked", $(this).prop("checked"));
-// });
-
-// $(".ch2").click(function () {
-//   if (!$(this).prop("checked")) {
-//     $(".ch-2").prop("checked", false);
-//   }
-// });
-//   Aquí termina la selección de celda
-// }
 
 /* Aquí termina la funcionalidad de php con js */
 
@@ -52,57 +23,74 @@ $(".chall").click(function () {
 
 function obtener_data() {
   var tabla_tbody = "";
-
+  var alm = "";
+  var alm_contador = "";
+  const contador = 1;
   $.ajax({
     type: "GET",
     url: url + "mostrar",
     // data: "data",
-    dataType: "json",  
+    dataType: "json",
     success: function (data) {
-        //Extraer data con map,forEach => prefiero esta forma 
-        data.map((datos,i) =>{
-            tabla_tbody += `
+      //Extraer data con map,forEach => prefiero esta forma
+      data.map((datos, i) => {
+        alm += "ch-" + datos.product_id + ",";
+        alm_contador += contador + i + ",";
+
+        tabla_tbody += `
         <tr>
-          <td><input class="chall ch-${datos.product_id}" id="nivel[0]-${i}" type="checkbox"  /></td>
-          <td><input class="chall ch${datos.product_id}" id="nivel[0][0]-${i}" type="checkbox"  /></td>
-          <td><input class="chall ch${datos.product_id}" id="nivel[0][1]-${i}" type="checkbox"  /></td>
-          <td><input class="chall ch${datos.product_id}" id="nivel[0][2]-${i}" type="checkbox"  /></td>
+        
+          <td><input class="chall ch-${cheq_pc(
+            datos.product_id
+          )} " id="nivel[0]-${i}" type="checkbox"  /></td>
+          <td><input class=" ch${cheq_pc(
+            datos.product_id
+          )}" id="nivel[0][0]-${i}" type="checkbox"  /></td>
+          <td><input class=" ch${cheq_pc(
+            datos.product_id
+          )}" id="nivel[0][1]-${i}" type="checkbox"  /></td>
+          <td><input class=" ch${cheq_pc(
+            datos.product_id
+          )}" id="nivel[0][2]-${i}" type="checkbox"  /></td>
             <td>
-                <a class="css-boton boton-primario" href="${url}product/edit/${datos.product_id}">Edit</a>
-                <a class="css-boton boton-advertencia" href="${url}product/delete/${datos.product_id}">Delete</a>
+                <a class="css-boton boton-primario" href="${url}product/edit/${
+          datos.product_id
+        }">Edit</a>
+                <a class="css-boton boton-advertencia" href="${url}product/delete/${
+          datos.product_id
+        }">Delete</a>
             </td>
         </tr>
             `;
-        });
+      });
 
-        // Extraer data con el map jquery 
-    //   $.map(data, function (datos, i) {
-    //     // console.log(datos.product_name);
-    //     tabla_tbody += `
-    //     <tr>
-    //         <td>${datos.product_id}</td>
-    //         <td>${datos.product_name}</td>
-    //         <td>${datos.product_price}</td>
-    //         <td>
-    //             <a class="css-boton boton-primario" href="${url}product/edit/${datos.product_id}">Edit</a>
-    //             <a class="css-boton boton-advertencia" href="${url}product/delete/${datos.product_id}">Delete</a>
-    //         </td>
-    //     </tr>
-    //         `;
-    //   });
+      //Lo almaceno en un arreglo y filtro la cadena vacía
+      let arr_contador = alm_contador.split(",").filter((x) => x);
+      let arr = alm.split(",").filter((x) => x);
+      console.log(arr_contador);
+      console.log(arr);
       $("#tabla_tbody").html(tabla_tbody);
     },
   });
 }
 
 obtener_data();
-/* Aquí termina la funcionalidad con js */
-$(".ch-2").click(function () {
-  $(".ch2").prop("checked", $(this).prop("checked"));
-});
+//Asociar eventos a elementos creados dinámicamente con jQuery
+function cheq_pc(id_chq) {
+  $(document).on("click", `.ch-${id_chq}`, function () {
+    $(`.ch${id_chq}`).prop("checked", $(this).prop("checked"));
+    if (!$(this).prop("checked")) {
+      $(`.ch${id_chq}`).prop("disabled", true);
+    }else{
+      $(`.ch${id_chq}`).prop("disabled", false);
+    }
+  });
 
-$(".ch2").click(function () {
-  if (!$(this).prop("checked")) {
-    $(".ch-2").prop("checked", false);
-  }
-});
+  $(document).on("click", `.ch${id_chq}`, function () {
+    if (!$(this).prop("checked")) {
+      $(`.ch-${id_chq}`).prop("checked", false);
+    }
+  });
+
+  return id_chq;
+}
